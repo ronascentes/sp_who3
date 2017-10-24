6d270721d83729cf5d63f3909e74a7f5c95a8525
ronascentes <ronascentes@gmail.com>
2017-10-24 15:56:43 -0200

    Support to in-memory

--- internals/healthCheck.sql
+++ internals/healthCheck.sql
@@ -9,19 +9,26 @@ https://github.com/Microsoft/tigertoolbox MSSQL Tiger team toolbox
 **********************************************************************************************************************************************************/
 USE [master]
 GO
+
+DECLARE @check_missing_indexes BIT, @check_top_queries BIT, @check_in_memory BIT;
+
 SET NOCOUNT ON;
 SET ANSI_WARNINGS ON;
 SET QUOTED_IDENTIFIER ON;
 SET DATEFORMAT mdy;
 SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
 
+SET @check_missing_indexes = 0;
+SET @check_in_memory = 1;
+SET @check_top_queries = 0;
+
 /************** SQL and OS version ************************************************************************************************************************/
-SELECT @@SERVERNAME AS [Server Name], @@VERSION AS [SQL Server and OS Version Info]
+SELECT @@SERVERNAME AS [Server Name], @@VERSION AS [SQL Server and OS Version Info];
 
 
 /************** Uptime information ************************************************************************************************************************/
-SELECT 'Uptime' AS [Category], sqlserver_start_time, CONVERT(VARCHAR(4),DATEDIFF(mi,sqlserver_start_time,GETDATE())/60/24) + 'd ' + CONVERT(VARCHAR(4),DATEDIFF(mi,sqlserver_start_time,GETDATE())/60%24) + 'hr ' + CONVERT(VARCHAR(4),DATEDIFF(mi,sqlserver_start_time,GETDATE())%60) + 'min' AS Uptime FROM sys.dm_os_sys_info (NOLOCK)
-GO
+SELECT 'Uptime' AS [Category], sqlserver_start_time, CONVERT(VARCHAR(4),DATEDIFF(mi,sqlserver_start_time,GETDATE())/60/24) + 'd ' + CONVERT(VARCHAR(4),DATEDIFF(mi,sqlserver_start_time,GETDATE())/60%24) + 'hr ' + CONVERT(VARCHAR(4),DATEDIFF(mi,sqlserver_start_time,GETDATE())%60) + 'min' AS Uptime FROM sys.dm_os_sys_info (NOLOCK);
+
 
 /************** AlwayOn information ************************************************************************************************************************/
 IF (SELECT SERVERPROPERTY('IsHadrEnabled')) = 1
@@ -42,15 +49,15 @@ ELSE
 	mirroring_role_desc, mirroring_partner_instance, mirroring_partner_name
 	FROM sys.databases a JOIN sys.database_mirroring b 
 	ON a.database_id = b.database_id
-	WHERE a.database_id > 4
-GO
+	WHERE a.database_id > 4;
+
 
 /************** ADVANCED SETTINGS ************************************************************************************************************************/
 -- priority boost (should be zero)
 SELECT 'Advanced_Settings' as [Category], name, value, value_in_use
 FROM sys.configurations WITH (NOLOCK)
-WHERE configuration_id IN (518,1538,1539,1544,1581,1517)
-GO
+WHERE configuration_id IN (518,1538,1539,1544,1581,1517);
+
 
 /************** Trace flags ************************************************************************************************************************/
 -- TF1118 turns off mixed page allocations. Preventing mixed page allocations reduces the risk of page latch contention 
@@ -71,7 +78,7 @@ GO
 -- Usage: 
 -- DBCC TRACEON (1118,3226,2371,4199, -1);
 IF EXISTS (select * from tempdb.sys.objects (NOLOCK) WHERE [object_id] = OBJECT_ID('tempdb.dbo.#trace_flag'))
-  DROP TABLE #trace_flag
+  DROP TABLE #trace_flag;
 
 -- Get the list of objects to defrag
 CREATE TABLE #trace_flag(
@@ -91,9 +98,8 @@ UNION
 SELECT TraceFlag, Status, Global, Session, N'changes to automatic update statistics' AS [INFO] FROM #trace_flag WHERE TraceFlag = 2371
 UNION
 SELECT TraceFlag, Status, Global, Session, N'SQL Server query optimizer hotfix servicing model https://support.microsoft.com/en-us/kb/974006 Enabled by default in SQL 2016' AS [INFO] FROM #trace_flag WHERE TraceFlag = 4199
-GO
-DROP TABLE #trace_flag
-GO
+DROP TABLE #trace_flag;
+
 
 /************** Task counts ************************************************************************************************************************/
 -- Get Average Task Counts (run multiple times)
@@ -106,8 +112,6 @@ AVG(runnable_tasks_count) AS [Avg Runnable Task Count],
 AVG(pending_disk_io_count) AS [AvgPendingDiskIOCount]
 FROM sys.dm_os_schedulers WITH (NOLOCK)
 WHERE scheduler_id < 255;
-GO
-
 
 -- Isolate top waits for server instance since last restart or wait statistics clear  (Query 34) (Top Waits)
 WITH [Waits] 
@@ -160,8 +164,7 @@ FROM Waits AS W1
 INNER JOIN Waits AS W2
 ON W2.RowNum <= W1.RowNum
 GROUP BY W1.RowNum
-HAVING SUM (W2.Percentage) - MAX (W1.Percentage) < 99 -- percentage threshold
-OPTION (RECOMPILE);
+HAVING SUM (W2.Percentage) - MAX (W1.Percentage) < 99; -- percentage threshold
 
 /************** CPU ************************************************************************************************************************/
 WITH DB_CPU_Stats
@@ -179,7 +182,6 @@ FROM DB_CPU_Stats
 WHERE DatabaseID > 4 -- system databases
 AND DatabaseID <> 32767 -- ResourceDB
 ORDER BY row_num;
-GO
 
 -- Get CPU trend for pass 256 min 
 declare @ts_now bigint
@@ -201,22 +203,8 @@ from (
             where ring_buffer_type = N'RING_BUFFER_SCHEDULER_MONITOR'
             and record like '%<SystemHealth>%') as x
       ) as y
-order by record_id desc
+order by record_id desc;
 
-
-/************** look for usually errors at errorlog ************************************************************************************************************************/
-exec xp_readerrorlog 0, 1, N'SQL Server is starting at high priority base',null,null,null, N'desc';
-exec xp_readerrorlog 0, 1, N'fatal exception ',null,null,null, N'desc';
-exec xp_readerrorlog 0, 1, N'Deadlock encountered',null,null,null, N'desc';
-exec xp_readerrorlog 0, 1, N'deadlock-list',null,null,null, N'desc';
-exec xp_readerrorlog 0, 1, N'An error has occurred while establishing a connection to the server',null,null,null, N'desc';
-exec xp_readerrorlog 0, 1, N'fatal error',null,null,null, N'desc';
-exec xp_readerrorlog 0, 1, N'corruption',null,null,null, N'desc';
-exec xp_readerrorlog 0, 1, N'corrupted',null,null,null, N'desc';
-exec xp_readerrorlog 0, 1, N'The error log has been reinitalized',null,null,null, N'desc';
-exec xp_readerrorlog 0, 1, N'SQL Server is starting',null,null,null, N'desc';
-exec xp_readerrorlog 0, 1, N'Recovery of database',null,null,null, N'desc';
-GO
 
 /************** Memory ************************************************************************************************************************/
 -- This query returns information about the state of the system (totals and available numbers) 
@@ -233,9 +221,7 @@ select 'Memory' as [Category], total_physical_memory_kb / 1048576 as phys_mem_gb
 	total_page_file_kb / 1048576 as total_page_file_gb,
 	available_page_file_kb / 1048576 as available_page_file_gb,
 	system_memory_state_desc
-from sys.dm_os_sys_memory WITH (NOLOCK) 
-GO
-
+from sys.dm_os_sys_memory WITH (NOLOCK); 
 
 -- SQL Server Process Address space info 
 -- (shows whether locked pages is enabled, among other things)
@@ -246,7 +232,6 @@ SELECT 'Memory' as [Category],physical_memory_in_use_kb,locked_page_allocations_
        available_commit_limit_kb, process_physical_memory_low, 
        process_virtual_memory_low
 FROM sys.dm_os_process_memory WITH (NOLOCK);
-GO
 
 SELECT N'Memory' as [Category], [object_name],counter_name, cntr_value, N'If Total < Target, SQL cannot grow the buffer pool due to memonry pressure. Total indicates the current size of the buffer poolFurther investigation is required.' AS [description]
 FROM sys.dm_os_performance_counters WITH (NOLOCK)
@@ -267,14 +252,55 @@ UNION
 SELECT N'Memory' as [Category], [object_name],counter_name, cntr_value, N'MGP > 0 for a sustained period is a very strong indicator of memory pressure' AS [description]
 FROM sys.dm_os_performance_counters WITH (NOLOCK)
 WHERE [object_name] LIKE N'%Memory Manager%' AND counter_name = N'Memory Grants Pending' 
-GO
 
 
--- Insufficient memory issues: http://support.microsoft.com/kb/309256 or https://support.microsoft.com/kb/2001221
-exec xp_readerrorlog 0, 1, N'A time out occurred while waiting for memory resources to execute the query',null,null,null, N'desc';
-exec xp_readerrorlog 0, 1, N'A There is insufficient system memory to run this query',null,null,null, N'desc';
-exec xp_readerrorlog 0, 1, N'A significant part of sql server memory has been paged out',null,null,null, N'desc';
-GO
+/************** IN-MEMORY ************************************************************************************************************************/
+IF @check_in_memory = 1
+	BEGIN
+	IF (SELECT @@microsoftversion / 0x01000000) > 11 -- greater than SQL 2012
+	BEGIN
+		IF (SELECT SERVERPROPERTY('IsXTPSupported')) = 1 -- in-memory OLTP is supported
+			BEGIN
+				DECLARE @varDbName SYSNAME, @dbContext NVARCHAR(500), @sql_str NVARCHAR(4000), @ParmDefinition1 NVARCHAR(500), @pDbXTPIsTrue BIT, @DbXTPIsTrue BIT;
+
+				-- Resource Pool utilization			
+				SET @sql_str = N'SELECT pool_id, Name, min_memory_percent, max_memory_percent, max_memory_kb/1024 AS max_memory_mb, 
+									used_memory_kb/1024 AS used_memory_mb, target_memory_kb/1024 AS target_memory_mb FROM sys.dm_resource_governor_resource_pools';
+				EXECUTE sp_executesql @sql_str
+
+				DECLARE curDbName CURSOR FAST_FORWARD FOR SELECT name FROM sys.databases WHERE database_id > 4;
+				OPEN curDbName;
+				FETCH NEXT FROM curDbName INTO @varDbName;
+				WHILE @@FETCH_STATUS = 0
+					BEGIN
+						SET @dbContext = @varDbName + N'..' + N'sp_executesql';
+						SET @ParmDefinition1 = N'@pDbXTPIsTrue BIT OUTPUT';
+						SET @sql_str = N'IF EXISTS (select 1 from sys.data_spaces where type = ''FX'') 
+										BEGIN SET @pDbXTPIsTrue = 1 END ELSE BEGIN SET @pDbXTPIsTrue = 0 END';
+
+						EXECUTE @dbContext @sql_str, @ParmDefinition1, @pDbXTPIsTrue = @DbXTPIsTrue OUTPUT;
+
+						IF @DbXTPIsTrue = 1
+						BEGIN
+							SET @sql_str = N'-- In memory table utilization
+											SELECT object_name(object_id) AS table_name, memory_allocated_for_table_kb/1024 memory_allocated_for_table_mb,
+											memory_used_by_table_kb/1024 memory_used_by_table_mb, memory_allocated_for_indexes_kb/1024 memory_allocated_for_indexes_mb,
+											memory_used_by_indexes_kb/1024 memory_used_by_indexes_mb FROM sys.dm_db_xtp_table_memory_stats (NOLOCK);
+
+											-- XTP usage (Same as XTP Memory Used in PERFMOM, counts Log pool utilization as well)
+											Select getdate() as DateTimeCaptured,sum(allocated_Bytes/(1024*1024)) As Total_XTP_Allocated_MB,
+											sum(Used_bytes/(1024*1024)) As Total_XTP_Used_MB,
+											(sum(allocated_Bytes/(1024*1024)) - sum(Used_bytes/(1024*1024))) As Free_XTP_Memory_MB
+											from sys.dm_db_xtp_memory_consumers (NOLOCK);';
+							EXECUTE @dbContext @sql_str
+						END;
+						FETCH NEXT FROM curDbName INTO @varDbName;
+					END; -- cursor
+				CLOSE curDbName;
+				DEALLOCATE curDbName;
+			END; -- in-memory OLTP is supported
+	END; -- greater than SQL 2012
+END; -- @check_in_memory
 
 
 /************** DISK ************************************************************************************************************************/
@@ -284,8 +310,7 @@ vs.logical_volume_name, CONVERT(DECIMAL(18,2),vs.total_bytes/1073741824.0) AS [T
 CONVERT(DECIMAL(18,2),vs.available_bytes/1073741824.0) AS [Available Size (GB)],  
 CAST(CAST(vs.available_bytes AS FLOAT)/ CAST(vs.total_bytes AS FLOAT) AS DECIMAL(18,2)) * 100 AS [Space Free %] 
 FROM sys.master_files AS f WITH (NOLOCK)
-CROSS APPLY sys.dm_os_volume_stats(f.database_id, f.[file_id]) AS vs
-GO
+CROSS APPLY sys.dm_os_volume_stats(f.database_id, f.[file_id]) AS vs;
 
 SELECT 'Data files in drive D' AS [Category], d.name as db_name, CAST((((size)/128.0)/1024) AS INT) as 'size in GB', physical_name
 FROM sys.master_files mf
@@ -293,15 +318,15 @@ JOIN sys.databases d
 ON mf.database_id = d.database_id
 where physical_name like 'D%'
 and d.name not in ('master','model','msdb','Dell_Maint')
-order by d.name
+order by d.name;
 
 SELECT 'Database size' AS [Category], d.name as [db_name], CAST((sum(((size)/128.0))/1024) AS INT) as [size in GB]
 FROM sys.master_files mf
 JOIN sys.databases d
 ON mf.database_id = d.database_id
 WHERE d.name not in ('master','model','msdb','Dell_Maint')
-GROUP BY d.name
-GO
+GROUP BY d.name;
+
 
 -- get I/O latency for all databases
 -- any value < 20 ms is acceptable for data files
@@ -314,12 +339,7 @@ INNER JOIN sys.master_files AS mf
 ON fs.database_id = mf.database_id
 AND fs.[file_id] = mf.[file_id]
 WHERE   num_of_reads > 0
-    AND num_of_writes > 0
-OPTION (RECOMPILE);
-GO
-
-exec xp_readerrorlog 0, 1, N'occurrence(s) of I/O requests taking longer than 15 seconds to complete on file',null,null,null, N'desc';
-GO
+    AND num_of_writes > 0;
 
 SELECT 'Tempdb_Usage' as [Category], SUM(user_object_reserved_page_count) AS user_object_pages,
         SUM(internal_object_reserved_page_count) AS internal_object_pages,
@@ -329,7 +349,6 @@ SELECT 'Tempdb_Usage' as [Category], SUM(user_object_reserved_page_count) AS use
         + SUM(version_store_reserved_page_count),
         SUM(unallocated_extent_page_count)/(128*1024) AS total_free_space_GB
 FROM   tempdb.sys.dm_db_file_space_usage WITH (NOLOCK);
-GO
 
 -- check last DBCC CHECKDB successfully
 IF OBJECT_ID('tempdb..#checkdb') IS NOT NULL 
@@ -351,10 +370,10 @@ UPDATE #checkdb SET DbName = N''?'' WHERE DbName IS NULL;';
 SELECT DISTINCT 'Last DBCC CHECKDB' as [Category], DBName, Value as [Last DBCC CHECKDB sucessfully]     
         FROM     #checkdb
         WHERE    Field = 'dbi_dbccLastKnownGood'
-		AND dbname NOT IN ('master','model','msdb','tempdb')
+		AND dbname NOT IN ('master','model','msdb','tempdb');
 
-DROP TABLE #checkdb
-GO
+DROP TABLE #checkdb;
+
 
 /************** System Health Session ************************************************************************************************************************/
 -- check system health session
@@ -418,4 +437,61 @@ ELSE
 BEGIN
 	SELECT 'No SystemHealth_Errors found' AS [Check]
 END;
-GO
+
+
+/************** look for usually errors at errorlog ************************************************************************************************************************/
+exec xp_readerrorlog 0, 1, N'SQL Server is starting at high priority base',null,null,null, N'desc';
+exec xp_readerrorlog 0, 1, N'fatal exception ',null,null,null, N'desc';
+exec xp_readerrorlog 0, 1, N'Deadlock encountered',null,null,null, N'desc';
+exec xp_readerrorlog 0, 1, N'deadlock-list',null,null,null, N'desc';
+exec xp_readerrorlog 0, 1, N'An error has occurred while establishing a connection to the server',null,null,null, N'desc';
+exec xp_readerrorlog 0, 1, N'fatal error',null,null,null, N'desc';
+exec xp_readerrorlog 0, 1, N'corruption',null,null,null, N'desc';
+exec xp_readerrorlog 0, 1, N'corrupted',null,null,null, N'desc';
+exec xp_readerrorlog 0, 1, N'The error log has been reinitalized',null,null,null, N'desc';
+exec xp_readerrorlog 0, 1, N'SQL Server is starting',null,null,null, N'desc';
+exec xp_readerrorlog 0, 1, N'Recovery of database',null,null,null, N'desc';
+exec xp_readerrorlog 0, 1, N'occurrence(s) of I/O requests taking longer than 15 seconds to complete on file',null,null,null, N'desc';
+-- Insufficient memory issues: http://support.microsoft.com/kb/309256 or https://support.microsoft.com/kb/2001221
+exec xp_readerrorlog 0, 1, N'A time out occurred while waiting for memory resources to execute the query',null,null,null, N'desc';
+exec xp_readerrorlog 0, 1, N'A There is insufficient system memory to run this query',null,null,null, N'desc';
+exec xp_readerrorlog 0, 1, N'A significant part of sql server memory has been paged out',null,null,null, N'desc';
+exec xp_readerrorlog 0, 1, N'memory',null,null,null, N'desc';
+exec xp_readerrorlog 0, 1, N'HK_E_OUTOFMEMORY',null,null,null, N'desc';
+exec xp_readerrorlog 0, 1, N'SQL Server is terminating because of a system shutdown',null,null,null, N'desc';
+exec xp_readerrorlog 0, 1, N'Always On: The availability replica manager is going offline',null,null,null, N'desc';
+exec xp_readerrorlog 0, 1, N'FAIL_PAGE_ALLOCATION',null,null,null, N'desc';
+
+IF @check_missing_indexes = 1
+BEGIN 
+	SELECT TOP 25
+	DB_NAME(dm_mid.database_id) AS Database_Name,
+	[User_Hits_on_Missing_Index] = (dm_migs.user_seeks + dm_migs.user_scans),
+	dm_migs.avg_user_impact, 							-- Query cost would reduce by this amount in percentage, on average.
+	dm_migs.avg_total_user_cost, 						-- Average cost of the user queries that could be reduced by the index in the group.
+	dm_migs.unique_compiles,							-- Number of compilations and recompilations that would benefit from this missing index group.
+	dm_migs.last_user_seek AS Last_User_Seek,
+	OBJECT_NAME(dm_mid.OBJECT_ID,dm_mid.database_id) AS [TableName],
+	'CREATE INDEX [IX_' + OBJECT_NAME(dm_mid.OBJECT_ID,dm_mid.database_id) + '_'
+	+ REPLACE(REPLACE(REPLACE(ISNULL(dm_mid.equality_columns,''),', ','_'),'[',''),']','') +
+	CASE
+	WHEN dm_mid.equality_columns IS NOT NULL AND dm_mid.inequality_columns IS NOT NULL THEN '_'
+	ELSE ''
+	END
+	+ REPLACE(REPLACE(REPLACE(ISNULL(dm_mid.inequality_columns,''),', ','_'),'[',''),']','')
+	+ ']'
+	+ ' ON ' + dm_mid.statement
+	+ ' (' + ISNULL (dm_mid.equality_columns,'')
+	+ CASE WHEN dm_mid.equality_columns IS NOT NULL AND dm_mid.inequality_columns IS NOT NULL THEN ',' ELSE
+	'' END
+	+ ISNULL (dm_mid.inequality_columns, '')
+	+ ')'
+	+ ISNULL (' INCLUDE (' + dm_mid.included_columns + ')', '') AS Create_Statement
+	FROM sys.dm_db_missing_index_groups dm_mig WITH (NOLOCK)
+	INNER JOIN sys.dm_db_missing_index_group_stats dm_migs WITH (NOLOCK)
+	    ON dm_migs.group_handle = dm_mig.index_group_handle
+	INNER JOIN sys.dm_db_missing_index_details dm_mid WITH (NOLOCK)
+	    ON dm_mig.index_handle = dm_mid.index_handle
+	WHERE dm_mid.database_ID > 4
+	ORDER BY Database_Name, dm_migs.avg_user_impact DESC;
+END;
